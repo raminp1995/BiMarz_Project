422038e3328ae569fe2c3ffb29ffe8b050e05d56
Ramin <ramin.pirnia95@gmail.com>
2024-04-22 23:58:42 +0330

    Added: RoleType and Default User Abilities Setter

---
+++ src/main/java/com/bemarzprj/constants/RoleType.java
@@ -0,0 +1,8 @@
+package com.bemarzprj.constants;
+
+public class RoleType
+{
+    public static final String OWNER = "OWNER";
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+}
